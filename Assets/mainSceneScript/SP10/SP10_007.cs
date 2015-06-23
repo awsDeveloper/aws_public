8bbf2ae73df6b9609da2e2698162560c1d36b9f8
shaK <shakueyes_487tan@yahoo.co.jp>
2015-06-23 14:46:16 +0900

    SP10データ追加

---
+++ Assets/mainSceneScript/SP10/SP10_007.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SP10_007 : MonoCard {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
