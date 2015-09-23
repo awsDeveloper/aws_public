5a875caaaf21fbdbe4d764e357a9f808b0f3c8cf
shaK <shakueyes_487tan@yahoo.co.jp>
2015-09-23 22:08:05 +0900

    ９段のデータインプット

---
+++ Assets/mainSceneScript/WX09/WX09_034.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WX09_034 : MonoCard {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
---
+++ Assets/mainSceneScript/WX09/WX09_048.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WX09_048 : MonoCard {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
